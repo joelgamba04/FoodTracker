4ea612dea9cf83810e55ca667b3eab3a2c6a6771
joelgamba04 <joelgamba04@gmail.com>
2026-02-25 07:52:07 +0800

    Change URL for staging

--- src/constants/apiRouteBaseURL.ts
+++ src/constants/apiRouteBaseURL.ts
@@ -1,1 +1,5 @@
-export const API_ROUTE_BASE_URL = "http://10.0.2.2:3000";
+// local backend testing
+// export const API_ROUTE_BASE_URL = "http://10.0.2.2:3000";
+
+// staging backend testing
+export const API_ROUTE_BASE_URL = "https://nutri-api.taguig-archive.online/";
