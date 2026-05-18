92303c336b88f92c9a712e6b5cd3d94e83e4f54f
joelgamba04 <joelgamba04@gmail.com>
2026-05-18 11:49:10 +0800

    Add adi-registration for Play Console signing

---
+++ plugins/withAdiRegistration.js
@@ -0,0 +1,28 @@
+// plugins/withAdiRegistration.js
+
+// plugin to add adi-registration.properties to the android manifest
+
+const { withDangerousMod } = require("@expo/config-plugins");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = function withAdiRegistration(config) {
+  return withDangerousMod(config, [
+    "android",
+    async (config) => {
+      const assetsDir = path.join(
+        config.modRequest.platformProjectRoot,
+        "app/src/main/assets",
+      );
+
+      fs.mkdirSync(assetsDir, { recursive: true });
+
+      fs.writeFileSync(
+        path.join(assetsDir, "adi-registration.properties"),
+        "DIYUSAFEFYOPUAAAAAAAAAAAAA",
+      );
+
+      return config;
+    },
+  ]);
+};
